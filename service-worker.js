2671d8957f812d0f13de560ebb0daf2fd24c8177
0xYach <161972417+0xYach@users.noreply.github.com>
2025-07-28 09:02:46 +0100

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,12 +9,25 @@ const OFFLINE_FILES = [
   "/book-tracker/icons/icon-512.png"
 ];
 
+// Install event: cache files
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_FILES))
   );
 });
 
+// Activate event: clean up old caches
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch event: serve cached files, fallback to network
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
